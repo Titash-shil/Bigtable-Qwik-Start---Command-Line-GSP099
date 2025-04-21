860cb9af31e863d6055462d8c517fddcd7db4160
QwikLab Explorers <155870248+Titash-shil@users.noreply.github.com>
2025-04-21 21:48:19 +0530

    Update qwiklab_explorers_gsp099.sh

--- qwiklab_explorers_gsp099.sh
+++ qwiklab_explorers_gsp099.sh
@@ -1,53 +1,17 @@
-#!/bin/bash
-BLACK_TEXT=$'\033[0;90m'
-RED_TEXT=$'\033[0;91m'
-GREEN_TEXT=$'\033[0;92m'
-YELLOW_TEXT=$'\033[0;93m'
-BLUE_TEXT=$'\033[0;94m'
-MAGENTA_TEXT=$'\033[0;95m'
-CYAN_TEXT=$'\033[0;96m'
-WHITE_TEXT=$'\033[0;97m'
-RESET_FORMAT=$'\033[0m'
-BOLD_TEXT=$'\033[1m'
-UNDERLINE_TEXT=$'\033[4m'
-clear
+echo project = `gcloud config get-value project` > ~/.cbtrc
 
+echo instance = quickstart-instance >> ~/.cbtrc
 
-# === Task 2: Connect to your instance (Configure cbt) ===
-echo "${CYAN_TEXT}Task 2: Configuring cbt...${RESET_FORMAT}"
-echo project = ${PROJECT_ID} > ~/.cbtrc
-echo instance = ${INSTANCE_ID} >> ~/.cbtrc
-echo "${GREEN_TEXT}~/.cbtrc configured.${RESET_FORMAT}"
-echo
+cbt createtable my-table
 
-# === Task 3: Read and write data ===
-echo "${CYAN_TEXT}Task 3: Working with table '${TABLE_NAME}'...${RESET_FORMAT}"
+cbt ls
 
-# Attempt to delete table first in case of prior partial run (optional)
-echo "${BLUE_TEXT}Attempting to delete table '${TABLE_NAME}' if it exists (ignore errors if not found)...${RESET_FORMAT}"
-cbt -project "${PROJECT_ID}" -instance "${INSTANCE_ID}" deletetable ${TABLE_NAME} || true # Suppress errors if table doesn't exist
+cbt createfamily my-table cf1
 
-echo "${BLUE_TEXT}Creating table '${TABLE_NAME}'...${RESET_FORMAT}"
-cbt -project "${PROJECT_ID}" -instance "${INSTANCE_ID}" createtable ${TABLE_NAME}
+cbt ls my-table
 
-echo "${BLUE_TEXT}Listing tables:${RESET_FORMAT}"
-cbt -project "${PROJECT_ID}" -instance "${INSTANCE_ID}" ls
+cbt set my-table r1 cf1:c1=test-value
 
-echo "${BLUE_TEXT}Creating column family '${COLUMN_FAMILY}'...${RESET_FORMAT}"
-cbt -project "${PROJECT_ID}" -instance "${INSTANCE_ID}" createfamily ${TABLE_NAME} ${COLUMN_FAMILY}
+cbt read my-table
 
-echo "${BLUE_TEXT}Listing column families:${RESET_FORMAT}"
-cbt -project "${PROJECT_ID}" -instance "${INSTANCE_ID}" ls ${TABLE_NAME}
-
-echo "${BLUE_TEXT}Writing data to '${TABLE_NAME}'...${RESET_FORMAT}"
-cbt -project "${PROJECT_ID}" -instance "${INSTANCE_ID}" set ${TABLE_NAME} r1 ${COLUMN_FAMILY}:c1="test-value"
-
-echo "${BLUE_TEXT}Reading data from '${TABLE_NAME}':${RESET_FORMAT}"
-cbt -project "${PROJECT_ID}" -instance "${INSTANCE_ID}" read ${TABLE_NAME}
-
-echo "${BLUE_TEXT}Deleting table '${TABLE_NAME}'...${RESET_FORMAT}"
-cbt -project "${PROJECT_ID}" -instance "${INSTANCE_ID}" deletetable ${TABLE_NAME}
-
-echo "${GREEN_TEXT}Table operations completed.${RESET_FORMAT}"
-
-set +e 
+cbt deletetable my-table
